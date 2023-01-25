efd0420a3453bd6735cc07087f88d1da5bb36de9
LeeMace <114292723+LeeMace@users.noreply.github.com>
2023-01-25 16:34:29 +0700

    [A] event manager added

--- Asteroids/Assets/Scripts/AppManager.cs
+++ Asteroids/Assets/Scripts/AppManager.cs
@@ -8,13 +8,17 @@ public class AppManager : Manager
     public GameManager gameManager { get; private set; }
     public MainMenuManager mainMenuManager { get; private set; }
 
+    public EventManager eventManager { get; private set; }
+
     [SerializeField] private GameObject sceneLoadingManagerPrefab;
     [SerializeField] private GameObject gameManagerPrefab;
     [SerializeField] private GameObject mainMenuManagerPrefab;
+    [SerializeField] private GameObject eventManagerPrefab;
 
     private GameObject sceneLoadingManagerObject;
     private GameObject gameManagerObject;
     private GameObject mainMenuManagerObject;
+    private GameObject eventManagerObject;
 
     private void Awake()
     {
@@ -23,18 +27,22 @@ public class AppManager : Manager
         sceneLoadingManagerObject = Instantiate(sceneLoadingManagerPrefab);
         gameManagerObject = Instantiate(gameManagerPrefab);
         mainMenuManagerObject = Instantiate(mainMenuManagerPrefab);
+        eventManagerObject = Instantiate(eventManagerPrefab);
 
         DontDestroyOnLoad(sceneLoadingManagerObject);
         DontDestroyOnLoad(gameManagerObject);
         DontDestroyOnLoad(mainMenuManagerObject);
+        DontDestroyOnLoad(eventManagerObject);
 
         sceneLoadingManager = sceneLoadingManagerObject.GetComponent<SceneLoadingManager>();
         gameManager = gameManagerObject.GetComponent<GameManager>();
         mainMenuManager = mainMenuManagerObject.GetComponent<MainMenuManager>();
+        eventManager = eventManagerObject.GetComponent<EventManager>();
 
         sceneLoadingManager.Setup(this);
         gameManager.Setup(this);
         mainMenuManager.Setup(this);
+        eventManager.Setup(this);
     }
 
     public override void Setup(Manager manager)
