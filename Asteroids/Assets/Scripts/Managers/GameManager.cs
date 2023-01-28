bd03f41de4a559a1d3cd95f2a2a1e553fcec28d6
Samuel Piggott <sjpiggott93@gmail.com>
2023-01-28 22:11:57 +0700

    Player setup

--- Asteroids/Assets/Scripts/Managers/GameManager.cs
+++ Asteroids/Assets/Scripts/Managers/GameManager.cs
@@ -7,6 +7,10 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : Manager
 {
+    [SerializeField] private GameObject playerPrefab;
+    private GameObject playerObject;
+    private PlayerBehaviour playerBehaviour;
+
     private AppManager appManager => manager as AppManager;
     private SceneLoadingManager sceneLoadingManager => appManager.sceneLoadingManager;
     private InputActions inputActions;
@@ -14,9 +18,14 @@ public class GameManager : Manager
     public override void Setup(Manager manager)
     {
         base.Setup(manager);
+        
         inputActions = new InputActions();
         inputActions.Enable();
         inputActions.AppControls.Menu.performed += OnMenuPressed;
+
+        playerObject = Instantiate(playerPrefab);
+        playerBehaviour = playerObject.GetComponent<PlayerBehaviour>();
+        playerBehaviour.Setup(manager);
     }
 
     private void OnMenuPressed(InputAction.CallbackContext obj)
