286c81a50941eb9551f4b67eb82c9e2539f71ed4
Samuel Piggott <sjpiggott93@gmail.com>
2023-01-29 12:46:15 +0700

    Player moving and wrapping round the screen

--- Asteroids/Assets/Scripts/Behaviours/PlayerBehaviour.cs
+++ Asteroids/Assets/Scripts/Behaviours/PlayerBehaviour.cs
@@ -8,6 +8,8 @@ public class PlayerBehaviour : BaseBehaviour
 {
     private PlayerData data;
     private int speed => data.GetSpeed();
+    private int rotationSpeed => data.GetRotationSpeed();
+    private int health; 
     private AppManager appManager => manager as AppManager;
     private GameManager gameManager => appManager.gameManager;
     private InputActions inputActions => gameManager.inputActions;
@@ -16,23 +18,45 @@ public class PlayerBehaviour : BaseBehaviour
     {
         base.Setup(manager);
         this.data = data;
-        inputActions.PlayerControl.Forward.performed += OnForwardPressed;
-        inputActions.PlayerControl.RotateLeft.performed += OnRotateLeftPressed;
-        inputActions.PlayerControl.RotateRight.performed += OnRotateRightPressed;
+        health = data.GetHealth();
     }
-
-    private void OnRotateRightPressed(InputAction.CallbackContext obj)
+    void Update()
     {
+        if (inputActions.PlayerControl.Forward.ReadValue<float>() > 0)
+        {
+            transform.position += transform.up * speed * Time.deltaTime;
+        }
+        if (inputActions.PlayerControl.RotateRight.ReadValue<float>() > 0)
+        {
+            transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+        }
+        if (inputActions.PlayerControl.RotateLeft.ReadValue<float>() > 0)
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+       
+        Vector3 screenPosition = Camera.main.WorldToViewportPoint(transform.position);
         
-    }
+        if (screenPosition.x < 0)
+        {
+            screenPosition.x = 1;
+        }
 
-    private void OnRotateLeftPressed(InputAction.CallbackContext obj)
-    {
-        
-    }
+        if (screenPosition.x > 1)
+        {
+            screenPosition.x = 0;
+        }
 
-    private void OnForwardPressed(InputAction.CallbackContext obj)
-    {
-        
+        if (screenPosition.y > 1)
+        {
+            screenPosition.y = 0;
+        }
+
+        if (screenPosition.y < 0)
+        {
+            screenPosition.y = 1;
+        }
+
+        transform.position = Camera.main.ViewportToWorldPoint(screenPosition); 
     }
 }
