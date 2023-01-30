a5a36a1f9f96e9214d1d4a9dd99b59afdaa0af87
Samuel Piggott <sjpiggott93@gmail.com>
2023-01-30 09:39:06 +0700

    Bullet is Base object =so it can wrap round screen

--- Asteroids/Assets/Scripts/Behaviours/BulletBehaviour.cs
+++ Asteroids/Assets/Scripts/Behaviours/BulletBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletBehaviour : BaseBehaviour
+public class BulletBehaviour : BaseObjectBehaviour
 {
   
 }
