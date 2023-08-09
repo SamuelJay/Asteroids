21839c01cf748e5c935a0658ee0ef71536c92557
LeeMace <ronsonjohnson44@gmail.com>
2023-08-09 12:26:58 +0700

    Asteroids: Assets: skybox and laser fire

---
+++ Asteroids/Assets/VolumetricLines/BuiltInRenderPipeline/ExampleScenes/ExampleScripts/ShotBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position += transform.forward * Time.deltaTime * 1000f;
+		
+		transform.position += new Vector3(transform.forward.x, transform.forward.y, 0) * Time.deltaTime * 1000f;
+	}
+}
