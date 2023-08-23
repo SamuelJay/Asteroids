9e34eaf15c0d63e2f1366b7644c8c901ecdf36ab
LeeMace <ronsonjohnson44@gmail.com>
2023-08-23 11:18:42 +0700

    Asteroids: transitions asset and main menu UI

--- Asteroids/Assets/Scripts/Behaviours/BulletBehaviour.cs
+++ Asteroids/Assets/Scripts/Behaviours/BulletBehaviour.cs
@@ -18,11 +18,26 @@ public class BulletBehaviour : BaseObjectBehaviour
         base.Update();
         Move();
         timer+=Time.deltaTime;
+        WrapPosition();
         
         if (timer > lifetime)
         {
             gameObject.SetActive(false);
             
+        }
+    }
+
+    private void WrapPosition() {
+
+        Vector3 screenPosition = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (screenPosition.x < 0 || screenPosition.x > 1 || screenPosition.y > 1 || screenPosition.y < 0)
+        {
+            gameObject.SetActive(false);
+        }
+        else {  
+            
+        transform.position = Camera.main.ViewportToWorldPoint(screenPosition);
         }
     }
 }
--- Asteroids/Assets/Scripts/Managers/SceneLoadingManager.cs
+++ Asteroids/Assets/Scripts/Managers/SceneLoadingManager.cs
@@ -1,3 +1,4 @@
+using EasyTransition;
 using System;
 using System.Collections;
 using System.Collections.Generic;
